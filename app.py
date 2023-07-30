9f8505e5ddb79b6a9e429a1a6737aa54a0269dd1
Ahmet Yusuf ALAN <cs.ahmetyusufalan@gmail.com>
2023-07-30 11:56:27 +0300

    docstring added

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def song_page(name, year):
         name (str): The name of the song.
         year (str): The year of the song.
     """
-    
+
     song_uri = smr.find_song_uri(name, year)
     formatted_song_uri = song_uri.split(':')[-1]
     uri_link = f'https://open.spotify.com/embed/track/{formatted_song_uri}?utm_source=generator'
@@ -24,6 +24,9 @@ def song_page(name, year):
 
 
 def spr_sidebar():
+    """
+    Renders the sidebar for the app and sets the app mode based on the selected option.
+    """
     menu = option_menu(
         menu_title=None,
         options=['Home', 'Results', 'About'],
@@ -39,8 +42,11 @@ def spr_sidebar():
     elif menu == 'About':
         st.session_state.app_mode = 'About'
 
-def home_page():
 
+def home_page():
+    """
+    Renders the Home page of the app, including input fields for the song, year, and song review.
+    """
     st.title("Spotify Music Recommender")
 
     col1, col2 = st.columns(2)
@@ -75,15 +81,27 @@ def home_page():
                 user_critic_text = critic_input
                 raw_recommendation_array = smr.get_recommendation_array(
                     song_input, year_input, number_cols, user_critic_text)
-                result = smr.format_chatgpt_recommendations (
+                result = smr.format_chatgpt_recommendations(
                     raw_recommendation_array, data, song_cluster_pipeline, 15)
-                st.session_state.song_uris = smr.get_recommendation_song_uri(result)
+                st.session_state.song_uris = smr.get_recommendation_song_uri(
+                    result)
                 st.write("You can access recommended song at result page")
             except:
-                st.write("An error occured please try again")
+                st.write("An error occurred please try again")
 
 
 def text_field(label, columns=None, **input_params):
+    """
+    Helper function to display a labeled text input field with the given label.
+
+    Args:
+        label (str): The label for the text input field.
+        columns (Optional[List[int]]): A list of column widths for layout purposes.
+        **input_params: Additional parameters to pass to the text_input function.
+
+    Returns:
+        str: The text entered in the text input field.
+    """
     c1, c2 = st.columns(columns or [1, 4])
 
     # Display field name with some alignment
@@ -98,6 +116,9 @@ def text_field(label, columns=None, **input_params):
 
 
 def find_random_song():
+    """
+    Finds and displays a random song using the Spotify API.
+    """
     try:
         song_input, year_input = smr.get_random_song()
         song_page(song_input, year_input)
@@ -107,6 +128,9 @@ def find_random_song():
 
 
 def result_page():
+    """
+    Renders the Results page of the app, displaying the recommended songs.
+    """
     if "song_uris" in st.session_state:
         for uri in st.session_state.song_uris:
             uri = uri.split(":")[-1]
@@ -115,14 +139,20 @@ def result_page():
             components.iframe(uri_link, height=80)
     else:
         st.subheader(
-            "Please enter song informations and review then click start prediction")
+            "Please enter song information and review, then click Start Prediction")
 
 
 def examples_page():
-    st.subheader("Will added")
+    """
+    Placeholder function for the Examples page (to be implemented if needed).
+    """
+    pass
 
 
 def About_page():
+    """
+    Renders the About page of the app, providing information about the development and credits.
+    """
     st.header('Development')
     """
     Have you ever listened to a song and liked it overall, but felt that certain features could be improved? Maybe the chorus was too loud, the energy level wasn't quite right, or there were either too many or too few words. I've had those experiences too, and that's what inspired me to create a song recommender based on user reviews.
@@ -132,8 +162,8 @@ def About_page():
     Although it's important to note that the dataset used for training the model was relatively small (170k), the recommender still aims to provide valuable suggestions. While it may not reach its full potential due to the limited data, it serves as a starting point for exploring new songs that align with your individual tastes.
 
     So, if you're looking to fine-tune your music experience and discover songs that better match your preferences, give this song recommender a try. Enter your review, and let the algorithm work its magic to recommend songs that you're more likely to enjoy.\n
-    Github : [alanahmet](https://github.com/alanahmet) \n
-    Mail : cs.ahmetyusufalan@gmail.com
+    Github: [alanahmet](https://github.com/alanahmet) \n
+    Mail: cs.ahmetyusufalan@gmail.com
     """
     st.subheader('Audio Features Explanation')
     """
@@ -145,7 +175,7 @@ def About_page():
     | Instrumentalness | Predicts whether a track contains no vocals. "Ooh" and "aah" sounds are treated as instrumental in this context. Rap or spoken word tracks are clearly "vocal". The closer the instrumentalness value is to 1.0, the greater likelihood the track contains no vocal content. Values above 0.5 are intended to represent instrumental tracks, but confidence is higher as the value approaches 1.0. |
     | Key | The key the track is in. Integers map to pitches using standard Pitch Class notation. E.g. 0 = C, 1 = C♯/D♭, 2 = D, and so on. If no key was detected, the value is -1. |
     | Liveness | Detects the presence of an audience in the recording. Higher liveness values represent an increased probability that the track was performed live. A value above 0.8 provides strong likelihood that the track is live. |
-    | Loudness | The overall loudness of a track in decibels (dB). Loudness values are averaged across the entire track and are useful for comparing relative loudness of tracks. Loudness is the quality of a sound that is the primary psychological correlate of physical strength (amplitude). Values typically range between -60 and 0 db. |
+    | Loudness | The overall loudness of a track in decibels (dB). Loudness values are averaged across the entire track and are useful for comparing relative loudness of tracks. Loudness is the quality of a sound that is the primary psychological correlate of physical strength (amplitude). Values typically range between -60 and 0 dB. |
     | Mode | Mode indicates the modality (major or minor) of a track, the type of scale from which its melodic content is derived. Major is represented by 1 and minor is 0. |
     | Speechiness | Speechiness detects the presence of spoken words in a track. The more exclusively speech-like the recording (e.g. talk show, audio book, poetry), the closer to 1.0 the attribute value. Values above 0.66 describe tracks that are probably made entirely of spoken words. Values between 0.33 and 0.66 describe tracks that may contain both music and speech, either in sections or layered, including such cases as rap music. Values below 0.33 most likely represent music and other non-speech-like tracks. |
     | Tempo | The overall estimated tempo of a track in beats per minute (BPM). In musical terminology, tempo is the speed or pace of a given piece and derives directly from the average beat duration. |
@@ -157,11 +187,14 @@ def About_page():
 
     st.subheader('Credit')
     """
-    Thanks for base of streamlit application to [abdelrhmanelruby](https://github.com/abdelrhmanelruby/Spotify-Recommendation-System) and dataset can be found [here](https://www.kaggle.com/datasets/vatsalmavani/spotify-dataset) 
+    Thanks for the base of the streamlit application to [abdelrhmanelruby](https://github.com/abdelrhmanelruby/Spotify-Recommendation-System), and the dataset can be found [here](https://www.kaggle.com/datasets/vatsalmavani/spotify-dataset) 
     """
 
 
 def main():
+    """
+    Main function to run the app. Renders different pages based on the app mode set in the sidebar.
+    """
     spr_sidebar()
     if st.session_state.app_mode == 'Home':
         home_page()
@@ -169,6 +202,7 @@ def main():
         result_page()
     if st.session_state.app_mode == 'About':
         About_page()
+
 
 if __name__ == '__main__':
     main()
